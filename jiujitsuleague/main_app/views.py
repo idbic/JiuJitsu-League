415ae558baaff801eb6208164e72396cd57b12b1
idbic <dbicga@gmail.com>
2022-07-24 11:21:34 -0500

    I added the home view

--- jiujitsuleague/main_app/views.py
+++ jiujitsuleague/main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Home View
+
+def home(request): 
+    return HttpResponse("hey it me Jiu jitsu League")
