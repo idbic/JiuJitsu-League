c40265771681eceed1b2ed1d915f93859dcdc8d0
idbic <dbicga@gmail.com>
2022-07-24 11:40:52 -0500

    set up first template for the tournaments view

--- jiujitsuleague/main_app/views.py
+++ jiujitsuleague/main_app/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Home View
 
 def home(request): 
     return HttpResponse("hey it me Jiu jitsu League")
 
 def tournaments(request): 
-    return HttpResponse("Tournaments Page Jiu Jitsu League")
+    return render(request, 'tournaments.html')
 
 def profile(request): 
     return HttpResponse("Settings/ Profile information")
