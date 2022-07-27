31deeff90cc7404c0aebcc218579b3cfa077a9aa
idbic <dbicga@gmail.com>
2022-07-27 08:31:44 -0500

    added url for auth

--- jiujitsuleague/main_app/urls.py
+++ jiujitsuleague/main_app/urls.py
@@ -14,5 +14,6 @@ path('tournaments/<int:tournaments_id>', views.tournaments_detail, name='detail'
 path('profile/', views.profile, name='profile'), 
 path('login/', views.login, name='login'),
 path('signup/', views.signup, name='signup'),
-path('rankings/', views.rankings, name='rankings')
+path('rankings/', views.rankings, name='rankings'),
+path('accounts/', include('django.contrib.auth.urls')),
 ]
