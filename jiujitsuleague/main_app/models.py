eb88a467c86ca97e0027a4ed7c03dec673acc571
idbic <dbicga@gmail.com>
2022-07-27 08:26:43 -0500

    updated tourney model and commented out and migrated the athlete model for now

--- jiujitsuleague/main_app/models.py
+++ jiujitsuleague/main_app/models.py
@@ -6,12 +6,16 @@ class Tournaments(models.Model):
     city = models.CharField(max_length=100, null=True)
     date = models.DateField(max_length=100, null=True)
     address = models.CharField(max_length=100, null=True)
-    prices = models.CharField(max_length=100, null=True)
-    athletes = models.CharField(max_length=100, null=True)
+    price = models.IntegerField(max_length=4, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Athlete(models.Model):
-    name = models.CharField(max_length=100, null=True)
-    age = models.IntegerField(max_length=100, null=True)
-    weight = models.IntegerField(max_length=100, null=True)
-    rank = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
+
+     
+# class Athlete(models.Model):
+#     name = models.CharField(max_length=100, null=True)
+#     age = models.IntegerField(max_length=100, null=True)
+#     weight = models.IntegerField(max_length=100, null=True)
+#     rank = models.CharField(max_length=100, null=True)
