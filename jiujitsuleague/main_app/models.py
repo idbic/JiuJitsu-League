ef8e1fddd533fab95e5967eb9a73a51bd1d7e9f3
idbic <dbicga@gmail.com>
2022-07-27 07:37:12 -0500

    added foreign key to the tournament model

--- jiujitsuleague/main_app/models.py
+++ jiujitsuleague/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tournaments(models.Model):
@@ -7,7 +8,7 @@ class Tournaments(models.Model):
     address = models.CharField(max_length=100, null=True)
     prices = models.CharField(max_length=100, null=True)
     athletes = models.CharField(max_length=100, null=True)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Athlete(models.Model):
     name = models.CharField(max_length=100, null=True)
