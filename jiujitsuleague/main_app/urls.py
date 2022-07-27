dde636fce8df316d87878000cdad54ff52139326
idbic <dbicga@gmail.com>
2022-07-27 07:52:35 -0500

    set up the url for the tournament detail page

--- jiujitsuleague/main_app/urls.py
+++ jiujitsuleague/main_app/urls.py
@@ -10,6 +10,7 @@ path('admin/', admin.site.urls),
 path("", views.front, name="front"), #this is the react url
 # path('', views.home, name='home'),
 path('tournaments/', views.tournaments, name='tournaments'),
+path('tournaments/<int:tournaments_id>', views.tournaments_detail, name='detail'),
 path('profile/', views.profile, name='profile'), 
 path('login/', views.login, name='login'),
 path('signup/', views.signup, name='signup'),
