0d9a2f83bf173680a38d60854af4029b618d7273
idbic <dbicga@gmail.com>
2022-07-28 10:04:00 -0500

    added url path for react

--- jiujitsuleague/main_app/urls.py
+++ jiujitsuleague/main_app/urls.py
@@ -1,4 +1,6 @@
+from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 from . import views
 from django.contrib import admin
 
@@ -9,7 +11,7 @@ urlpatterns = [
 path('admin/', admin.site.urls),
 path("", views.front, name="front"), #this is the react url
 # path('', views.home, name='home'),
-path('tournaments/', views.tournaments, name='tournaments'),
+path('tournaments/', Reactview.as_view(), name='tournaments'),
 path('tournaments/<int:tournaments_id>', views.tournaments_detail, name='detail'),
 path('profile/', views.profile, name='profile'), 
 path('login/', views.login, name='login'),
