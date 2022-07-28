b25e342550305d42c9cb8f9f2e4083192ce699e7
idbic <dbicga@gmail.com>
2022-07-28 09:59:15 -0500

    updated views with a class that will help render python data to json

--- jiujitsuleague/main_app/views.py
+++ jiujitsuleague/main_app/views.py
@@ -32,3 +32,28 @@ def signup(request):
 
 def rankings(request):
     return render(request, "rankings.html")
+
+
+
+from rest_framework.views import APIView
+from . models import *
+from rest_framework.response import Response
+from . serializer import *
+# Create your views here.
+  
+class ReactView(APIView):
+    
+    serializer_class = ReactSerializer
+  
+    def get(self, request):
+        detail = [ {"city": detail.city,"date": detail.date, "address": detail.address, "price": detail.price
+        "user": detail.user} 
+        for detail in React.objects.all()]
+        return Response(detail)
+  
+    def post(self, request):
+  
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return  Response(serializer.data)
